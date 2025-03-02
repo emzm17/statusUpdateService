b40e1cb39b4999a0662af2bee6ff5d0d9bb3e8b0
emzm17 <dibya172000@yahoo.com>
2025-03-02 17:54:55 +0530

    envs added

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: '.env.production' });
+} else {
+    dotenv.config({ path: '.env.development' });
+}
 
 
 export const REDIS_HOST = process.env.REDIS_HOST;
